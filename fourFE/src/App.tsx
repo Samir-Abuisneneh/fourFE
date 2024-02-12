bbb08026f73ca88ec2adebf07c89048a58ad8535
Omar AbuRish <191020@ppu.edu.ps>
2024-02-12 20:30:23 +0200

    stripe

--- fourFE/src/App.tsx
+++ fourFE/src/App.tsx
@@ -1,47 +1,56 @@
-import { Typography, Button, Link, Grid } from '@mui/material';
-import { GameTitle, HeroSectionTitleContainer, HomePageWrapper } from './components';
-import HeroImage from "./assets/hero-section-image.svg"
+import LoginScreen from './login.tsx';
+import RegScreen from './register.tsx';
+import GamePage from './play.tsx';
+import Leaderboard from './leaderboard.tsx';
+import LoginScreen2 from './login2.tsx';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+import Payment from './stripe/Payment.tsx'
+import Completion from './stripe/Completion.tsx'
+
+import {loadStripe} from '@stripe/stripe-js';
+import {useEffect, useState} from 'react';
+import Home from './home.tsx'
 import './App.css'
 
 const App = () => {
+
+  const [ stripePromise, setStripePromise ] = useState(null);
+
+  useEffect(() => {
+
+    const storedToken = localStorage.getItem("token");
+
+    fetch("http://localhost:8080/api/payment/config", {
+      headers: {
+          // "Content-Type": "application/json",
+          Authorization: `Bearer ${storedToken}`,
+      },
+    }).then(async (r) => {
+      const { publishableKey } = await r.json();
+      setStripePromise(loadStripe(publishableKey));
+    });
+  }, []);
+
+
   return (
-    <>
-      <HomePageWrapper>
-        <Grid style={{ width: '50%' }} container spacing={2} columns={16}>
-          <Grid item xs={8}>
-            <HeroSectionTitleContainer>
-              <Typography variant="h2" component="h1" gutterBottom>
-                Welcome to <GameTitle variant='h2' color="primary.main"> Four</GameTitle> Game
-              </Typography>
-              <Typography color={'grey'} variant="h5" paragraph>
-                Start playing and have fun
-              </Typography>
-              <div>
-                <Link href="/login">
-                  <Button variant="contained" color="primary" size="large" style={{ marginRight: '16px' }}>
-                    Login
-                  </Button>
-                </Link>
-                <Link href="/register">
-                  <Button variant="outlined" color="primary" size="large">
-                    Register
-                  </Button>
-                </Link>
-              </div>
-            </HeroSectionTitleContainer>
-          </Grid>
-          <Grid item xs={8}>
-            <img src={HeroImage} height={400} />
-          </Grid>
-        </Grid>
-        <div className="custom-shape-divider-bottom-1707520781">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-          </svg>
-        </div>
-      </HomePageWrapper>
-    </>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/login2" element={<LoginScreen2 />} />
+        <Route path="/register" element={<RegScreen />} />
+        <Route path="/play" element={<GamePage />} />
+        <Route path='/leaderboard' element={<Leaderboard />} />
+        <Route path="/payment/pay" element={<Payment stripePromise={stripePromise} />} />
+        <Route path="/payment/completion" element={<Completion stripePromise={stripePromise} />} />
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
-export default App
+export default App;
---
+++ fourFE/src/stripe/Payment.tsx
@@ -0,0 +1,37 @@
+import {useEffect, useState} from 'react';
+
+import {Elements} from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm'
+
+function Payment(props) {
+  const { stripePromise } = props;
+  const [ clientSecret, setClientSecret ] = useState('');
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    const storedToken = localStorage.getItem("token");
+
+    fetch("http://localhost:8080/api/payment/create-payment-intent", {
+        headers: {
+            // "Content-Type": "application/json",
+            Authorization: `Bearer ${storedToken}`,
+        },
+    })
+      .then((res) => res.json())
+      .then(({clientSecret}) => setClientSecret(clientSecret));
+  }, []);
+
+
+  return (
+    <>
+      <h1>Payment</h1>
+      {clientSecret && stripePromise && (
+        <Elements stripe={stripePromise} options={{ clientSecret, }}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </>
+  );
+}
+
+export default Payment;
